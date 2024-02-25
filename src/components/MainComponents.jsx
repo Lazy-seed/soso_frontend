ea8d00d34a2d1437158aa011ab3abb1d425b031b
aryankoko <rkoko5095@gmail.com>
2024-02-25 08:23:48 +0530

    add - component file

---
+++ src/components/MainComponents.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import proflile from './images/proflile.png'
+
+export const FollowList = () => {
+    return (
+        <div>
+            <img src={proflile} width={50} alt="" />
+
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,25 +2,26 @@ import React from 'react'
 import Home from './home/Home'
 import TopNav from '../utilities/TopNav'
 import { Col, Container, Row } from 'reactstrap'
+import { FollowList } from '../components/MainComponents'
 
 export default function HomePages() {
   return (
     <div>
       <TopNav />
-      <Row className='mx-0' style={{minHeight:"400px"}}>
+      <Row className='mx-0' style={{ minHeight: "400px" }}>
 
         {/* left screen */}
         <Col md="2" className='m-0 border'></Col>
 
         {/* main screen */}
         <Col md="" className='m-0 border border-danger'>
-        <Home />
+          <Home />
 
         </Col>
 
         {/* right screen */}
         <Col md="3" className='m-0 border'>
-
+          <FollowList />
         </Col>
       </Row>
     </div>
